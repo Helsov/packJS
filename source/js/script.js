b122ff33112211c8022fcc901e0eae6b0b76af78
Klepikov Vladislav <vl_klepikov@mail.ru>
2018-10-03 09:55:50 +0400

    Update

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,40 @@
-window.onload = function(){
-    class Men {
-        constructor(name, familia){
-            this.name = name;
-            this.familia = familia;
-        }
+class Car {
+    constructor(color, year, manufacturer, engine) {
+        this.color = color;
+        this.year = year;
+        this.manufacturer = manufacturer;
+        this.engine = engine;
+    };
 
-        beg(){
-            console.log('11111');
-        }
+    startEngine() {
+        this.engine.on();
+    };
 
+    setColor(color) {
+        if (typeof (color) == "string") {
+            this.color = color;
+        } else {
+            console.log('Ne stroka');
+        }
+    };
+
+    getColor() {
+        return this.color;
+    };
+}
+
+class Engine {
+    on() {
+        console.log('Двигатель включен');
     }
-};
+
+    off() {
+        console.log('Двигатель выключен');
+    }
+}
+
+engine = new Engine();
+
+myCar = new Car('white', 2015, 'Mersedes', engine);
+myCar.setColor('BLack');
+myCar.startEngine();
